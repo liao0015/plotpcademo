4186ba5b67a389e57455e8e839cdf0013da59d1f
liao0015 <liao0015@algonquinlive.com>
2017-03-10 16:53:00 -0500

    kk

--- R/plot_pca_demo.R
+++ R/plot_pca_demo.R
@@ -4,7 +4,7 @@
 #' @export
 #' @param input1 experimental design input
 #' @param input2 protein groups input
-plot_pca<-function(fooinput1, fooinput2, ...){
+plot_pca_demo<-function(fooinput1, fooinput2, ...){
 
 ExperimentalDesign<-fooinput1
 proteinGroups<-fooinput2
